1aef96c511cc7e12d59e9a57f53a50a93c76a1c7
Randolh <estuardotecun@gmail.com>
2026-04-27 15:46:46 -0600

    feat: :sparkles: add function delete product
    this function deletes products checking disponibiliti and stock before that

---
+++ funciones.py
@@ -0,0 +1,87 @@
+from json import dumps, load, JSONDecodeError
+
+archivo_productos = "coffee.json"
+
+
+def leer_json(archivito):
+    respuesta = {}
+    try:
+        with open(archivito, "r")as archivo:
+            respuesta = load(archivo)
+            return respuesta
+    except FileNotFoundError:
+        print("Archivo no encontrado, se creará uno nuevo al guardar el primer producto.")
+        return respuesta
+    except JSONDecodeError:
+        print("Archivo vacío o con formato incorrecto, se creará uno nuevo al guardar el primer producto.")
+        return respuesta
+    
+def escribir_json(archivito, contenido):
+    with open(archivito, "w")as archivo:
+        guardar = dumps(contenido, indent=4)
+        archivo.write(guardar)
+
+
+def eliminar_producto():
+    while True:
+        print("\n\n---- Eliminar Productos Descontinuados ----\n")
+        productos = leer_json(archivo_productos)
+
+        if not productos:
+            print("No hay productos en el inventario.")
+            return
+        
+        productos_sin_dispo = {nombre: valores for nombre, valores in productos.items() if not valores["disponibilidad"]}
+        list_keys = [nombre for nombre, valores in productos.items() if not valores["disponibilidad"]]
+
+        if len(productos_sin_dispo) == 0:
+            print("No hay productos descontinuados para eliminar.")
+            return
+        
+        for i, nombre in enumerate(productos_sin_dispo, start=1):
+            caffe_p = productos[nombre]
+
+            nombre = caffe_p["descripcion"]
+            dispo = caffe_p["disponibilidad"]
+            stock = caffe_p["stock"]
+
+            print(f'{i}. {nombre} - Stock: {stock} | Disponibilidad: {"Si" if dispo else "No"}')
+
+        opt = input("\nIngrese el el numero de producto que desea elimminar: ")
+
+        try:
+            opt = int(opt)
+        except ValueError:
+            print("\nSolo puedes ingresar numeros...")
+            input("Presione (Enter) para continuar...")
+            continue
+
+        if opt <= 0 or opt > len(productos_sin_dispo):
+            print("\nOpción no disponible!")
+            input("Presione (Enter) para continuar...")
+            continue
+
+        valor_s = productos[list_keys[opt - 1]]["stock"]    
+
+        if valor_s > 0:
+            while True:
+                eliminar = input(f"\nEl producto aun tiene stock de {valor_s}, deseas eliminarlo (s/n): ").strip()
+
+                if eliminar == "s":
+                    productos.pop(list_keys[opt - 1])
+
+                    escribir_json(archivo_productos, productos)
+
+                    print("\nProducto Eliminado Correctamente!")
+                    input("Presione (Enter) para continuar...")
+                    return
+                elif eliminar == "n":
+                    print("\nEliminación Cancelada!")
+                    input("Presione (Enter) para continuar...")
+                    return
+                else:
+                    print("\nOpción no disponible!")
+                    input("Presione (Enter) para continuar...")
+                    continue
+    
+eliminar_producto()
